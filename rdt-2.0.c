b911664ba661c7ba3171934ce257184d2e57e493
d50100 <155054664+d50100@users.noreply.github.com>
2024-01-24 08:54:21 +0800

    Update rdt-2.0.c

--- rdt-2.0.c
+++ rdt-2.0.c
@@ -50,12 +50,20 @@ int is_all_ones(uint16_t value){
 
 typedef unsigned int UINT32;
 
+enum PacketType {
+    CMD_PUSH,
+    CMD_ACK, 
+    CMD_NAK
+} PacketType;
+
+
 // 协议头部
 struct PROTO_HEADER {
     UINT32 sn;   // 分组中加序号, 防止重复
     UINT32 cmd;  // 用于区别命令和数据
     UINT32 len;  // 数据长度
     uint16_t chksum; // 检验和
+    uint8_t pkt_type;  // 包类型
     char data[1];
 };
 
@@ -76,39 +84,44 @@ void udt_recv(int fd, char* packet, UINT32* len){
 
 #define IOFFSET(TYPE, MEMBER) ((size_t) (&((TYPE *)0)->MEMBER))
 
-void make_pkt(char* packet, UINT32* packet_length, char* data, UINT32 data_len){
+void make_pkt(char* packet, UINT32* packet_length, enum PacketType pkt_type, char* data, UINT32 data_len){
     // 将头部字段填充到 packet
     PROTO_HEADER proto_header;
     memset(&proto_header, 0, sizeof(PROTO_HEADER));
-    char* pos = packet;
+    
+    proto_header.pkt_type = pkt_type;
 
+    char* pos = packet;
     // todo (1) 协议字段填充
     pos = encode_proto_header(packet, &proto_header);
-
-    // payload 填充
-    memcpy(pos, data, data_len); 
 
     // 计算包的总长度
     *packet_length = sizeof(PROTO_HEADER) + data_len;
 
     // 计算检验和
-    uint16_t chk = calculate_checksum(packet, *packet_length);
+    uint16_t chk = calculate_checksum(packet, packet_length);
 
+    // 检验和填充
+    checksum_fill((uint32_t)packet, chk);
+
+}
+void checksum_fill(uint8_t packet , uint16_t chk){
     size_t offset = IOFFSET(PROTO_HEADER, chksum);
 
     *((uint8_t *)packet + offset ) = (chk & 0xFF00) >> 8;
     *((uint8_t *)packet + offset + 1) = chk & 0x00FF;
-
 }
 
 // 对上层提供服务
-void rdt_send(int fd, char* data, UINT32 data_len) {
+void rdt_send(int fd, char* data, enum PacketType pkt_type, UINT32 data_len) {
      
     char packet[1400] = {0};
     UINT32 packet_length = 0;
 
     // 封包
-    make_pkt(packet, &packet_length, data, data_len);
+    make_pkt(packet, &packet_length, pkt_type, data, data_len);
+
+    // 数据发送
     udt_send(fd, packet, packet_length);
 }
 
@@ -145,8 +158,28 @@ void rdt_recv(int fd, char* data, UINT32 *data_len){
     // 使用下层的服务接受数据
     udt_recv(fd, packet, &packet_length);
 
+    // 校验和校验
+    uint16_t chk = calculate_checksum(packet, packet_length);
+
+    if (is_all_ones(chk)){
+        // 校验正确 发送 ACK
+        char ack_pkt[1000] = {0};
+        UINT32 ack_len = sizeof(ack_pkt);
+        make_pkt(ack_pkt, &ack_len, CMD_ACK, NULL, 0);
+        udt_send(fd, packet, packet_length);
+    }
+    else {
+        // 校验错误 发送 NAK
+        char nak_pkt[1000] = {0};
+        UINT32 nak_len = sizeof(nak_pkt);
+        make_pkt(nak_pkt, &nak_len, CMD_NAK, NULL, 0);
+        udt_send(fd, packet, packet_length);
+    }
+
     // 解包
     extract_pkt(packet, packet_length, data, data_len);
+
+
 }
 
 void sender(){
@@ -154,7 +187,12 @@ void sender(){
     int fd = 3;
     char data[100] = "send data";
     printf("send data: %s\n", data);
-    rdt_send(fd, data, strlen(data));
+
+    enum PacketType pkt_type = CMD_PUSH;
+    rdt_send(fd, data, pkt_type, strlen(data));
+
+    // 等待 ACK 或 NAK
+    rdt_recv(fd, data, NULL);
 }
 
 void receiver(){
